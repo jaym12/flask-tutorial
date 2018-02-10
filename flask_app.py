8aaffc67a3effb36a10caaf23e5619775db476b7
jayem <jaydevdesign@gmail.com>
2018-02-10 10:02:30 -0600

    added data.json and the price of chair package

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,10 @@
-__author__= 'jdevdesign'
+__author__ = 'jdevdesign'
 
 
-user_age = input("What is your age?")
-age_seconds = int(user_age) * 365 * 24 * 60 * 60
-print('Your age in seconds is {}'.format(age_seconds))
+def age_app():
+    user_age = input("What is your age?")
+    age_seconds = int(user_age) * 365 * 24 * 60 * 60
+    print('Your age in seconds is {}'.format(age_seconds))
+
+
+age_app()
