5f4c7273a157b8558d24b40ec2582a991800f962
jayem <jaydevdesign@gmail.com>
2018-02-10 09:48:40 -0600

    removed flask, ran successfully

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
-import flask from Flask
+__author__= 'jdevdesign'
+
 
 user_age = input("What is your age?")
 age_seconds = int(user_age) * 365 * 24 * 60 * 60
-print("Your age in seconds is {}".format(age_seconds))
+print('Your age in seconds is {}'.format(age_seconds))
